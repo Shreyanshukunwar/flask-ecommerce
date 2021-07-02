26ad57900834acc5cb11d935ae546a727cdebaf8
Shreyanshukunwar <shreyanshukunwar@gmail.com>
2021-07-02 10:24:12 +0545

    adding flask forms

--- market/__init__.py
+++ market/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 #Configs for db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '542999b3772800fd586b0ee4'
 
 db = SQLAlchemy(app)
 
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,8 @@
 from market import app
 from flask import render_template
+
 from .models import Item
+from .forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,3 +13,8 @@ def home_page():
 def market_page():
 	items = Item.query.all()
 	return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+	form = RegisterForm()
+	return render_template('register.html', form=form)
