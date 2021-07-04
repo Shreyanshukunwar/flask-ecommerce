23bde64df8a8d7422abf4c80dbdcc03773f636c3
Shreyanshukunwar <shreyanshukunwar@gmail.com>
2021-07-04 17:29:33 +0545

    hide secret key

--- market/__init__.py
+++ market/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+from .config import secrets
+
 app = Flask(__name__)
 
 #Configs for db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '542999b3772800fd586b0ee4'
+app.config['SECRET_KEY'] = secrets['secret_key']
 
 db = SQLAlchemy(app)
 
@@ -16,5 +18,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
+
+# print("SECRET KEY", str(secrets['secret_key']))
 
 from market import routes
