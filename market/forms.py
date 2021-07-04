beac05a027ba9a9392ce4333a9b42a82a7d11d00
Shreyanshukunwar <shreyanshukunwar@gmail.com>
2021-07-04 11:57:41 +0545

    advanced registration validations

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+
+from market.models import User
 
 class RegisterForm(FlaskForm):
+
+	def validate_username(self, username_to_check):
+		user_check = username_to_check.data
+		user = User.query.filter_by(username=user_check).first()
+		if user is not None:
+			raise ValidationError('Username already exists. Please use another Username')
+
+	def validate_email_address(self, email_to_check):
+		user = User.query.filter_by(email_address=email_to_check.data).first()
+		if user is not None:
+			raise ValidationError('Email already exists. Please use another.')
+
 	username = StringField(label='Username', validators=[Length(min=2, max=20), DataRequired()])
 	email_address = StringField(label='Email', validators=[Email(), DataRequired()])
 	password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
